a4588117160e14554cf2bbfc4d81cfc8de962efe
delta-ksi <delta.ksi42@gmail.com>
2025-09-15 19:08:32 +0300

    implement one task, change begin part of tasks src files

---
+++ tasks/task___001___one_simple_thread.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void* routine() {
+    printf("Test from threads\n");
+}
+
+int main(int argc, char** argv) {
+    pthread_t t1;
+    pthread_create(&t1, NULL, &routine, NULL);
+    pthread_join(t1, NULL);
+    return 0;
+}
